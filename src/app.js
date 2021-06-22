db79681270ec8171ead5651fd09219870fcd5693
Paulo Moss <paulo.moss@poli.ufrj.br>
2021-06-22 11:02:59 -0300

    rotas de login e cadastro com database

--- src/app.js
+++ src/app.js
@@ -1,12 +1,48 @@
 import express from 'express';
 import cors from 'cors';
 
+import connection from './database.js';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/teste", async (req,res) => {
-    res.send('ok')
+app.post("/login", async (req,res) => {
+
+    const { email, password } = req.body
+    try{
+        const result = await connection.query('SELECT * FROM clients WHERE name = $1 AND password = $2', [email, password]);
+        if(!result.rows.length){
+            res.sendStatus(400)
+        }
+        res.send(result.rows);
+    }catch(e){
+        console.log(e);
+        res.sendStatus(400);
+    }
+})
+
+app.post("/sign-up", async (req,res) => {
+
+    const { name, email, password, confirmPassword } = req.body;
+    try{
+        await connection.query('INSERT INTO clients (name, password) VALUES ($1, $2)', [name, password]);
+        res.sendStatus(200);
+
+    }catch(e){
+        console.log(e);
+        res.sendStatus(400);
+    }
+})
+
+app.get("/clients", async (req,res) => {
+    try{
+    const result = await connection.query('SELECT * FROM clients');
+    res.send(result.rows)
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(400)
+    }
 })
 
 app.listen(4000, () => {
