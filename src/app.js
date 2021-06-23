92adffc35ba1310af88cb3342db22d7310b898ad
Paulo Moss <paulo.moss@poli.ufrj.br>
2021-06-23 19:23:27 -0300

    inserindo extrato no bd + calculo de saldo

--- src/app.js
+++ src/app.js
@@ -57,8 +57,9 @@ app.post("/login", async (req,res) => {
                 VALUES ($1, $2)
             `, [user.id, token]);
 
-            const { name, email } = user;
-            res.send({name, email, token});
+            delete user.password;
+
+            res.send({id, email, token});
 
         } else {
             res.sendStatus(401);
@@ -71,15 +72,72 @@ app.post("/login", async (req,res) => {
 
 app.get("/records", async (req,res) => {
     try{
-    const authorization = req.headers['authorization'];
-    const token = authorization.replace('Bearer ', '');
-    const result = await connection.query(`
-        SELECT records.value, records.withdraw, records.date, records.description FROM records
-        JOIN sessions
-        ON sessions."userId" = records."userId"
-        WHERE sessions.token = $1
+        const authorization = req.headers['authorization'];
+        const token = authorization?.replace('Bearer ', '');
+
+        if(!token) return res.sendStatus(401);
+        
+        const success = await connection.query(`
+            SELECT * FROM sessions 
+            WHERE token = $1
+        `, [token]);
+        
+        if(success.rows[0]) {
+            const result = await connection.query(`
+            SELECT records.id, records.value, records.type, records.date, records.description FROM records
+            JOIN sessions
+            ON sessions."userId" = records."userId"
+            WHERE sessions.token = $1
+        `, [token]);
+        
+        let balance = result.rows.reduce((acc, item) => item.type === 'Incomming' ? acc + item.value : acc - item.value , 0);
+        balance = (balance/100).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+
+        result.rows.forEach(r => r.value = (r.value/100).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }));
+
+        res.send({userId: success.rows[0].userId, balance, records: result.rows})
+
+        } else {
+            res.sendStatus(401)
+        }
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500)
+    }
+})
+
+app.post("/records/:transferType", async (req,res) => {
+    try{
+        const authorization = req.headers['authorization'];
+        const token = authorization?.replace('Bearer ', '');
+
+        if(!token) return res.sendStatus(401);
+
+        const { transferType } = req.params;
+        const { value, description } = req.body;
+        let type;
+        if(transferType === 'Out') {
+            type = 'Withdraw';
+        } else {
+            type = 'Incomming';
+        }
+
+        const success = await connection.query(`
+        SELECT * FROM sessions 
+        WHERE token = $1
     `, [token]);
-    res.send(result.rows)
+        
+        if(success.rows[0]) {
+            const userId = success.rows[0].userId
+            await connection.query(`
+                INSERT INTO records ("userId", value, type, date, description)
+                VALUES ($1, $2, $3, NOW(), $4 )
+            `, [userId, value, type, description]);
+        } else {
+            return res.sendStatus(401);
+        }
+        res.sendStatus(201);
+
     } catch(e) {
         console.log(e);
         res.sendStatus(500)
